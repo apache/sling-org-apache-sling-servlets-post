951f38e844a6c04daf3b6284baf650938eb3a0a0
Rahul Bhardwaj <rahubhar@adobe.com>
2018-01-03 16:01:13 +0530

    SLING-7346-Fixing issue related to Json object in post response not getting parsed properly

--- src/main/java/org/apache/sling/servlets/post/JSONResponse.java
+++ src/main/java/org/apache/sling/servlets/post/JSONResponse.java
@@ -24,6 +24,7 @@ import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -103,9 +104,12 @@ public class JSONResponse extends AbstractPostResponse {
         JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
         for (Map.Entry<String, Object> entry : json.entrySet()) {
             if (entry.getValue() != null) {
-                jsonBuilder.add(entry.getKey(), entry.getValue().toString());
-            }
-            else {
+                if(entry.getValue().getClass().getClassLoader() != null) {
+                    jsonBuilder.add(entry.getKey(), Json.createReader(new StringReader(entry.getValue().toString())).readObject());
+                } else {
+                    jsonBuilder.add(entry.getKey(), entry.getValue().toString());
+                }
+            } else {
                 jsonBuilder.addNull(entry.getKey());
             }
         }
