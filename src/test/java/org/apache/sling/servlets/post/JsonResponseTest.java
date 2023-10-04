c881c4856216f05244f8297dfa09b144db3964af
Ashok Pelluru <ashokmca07@gmail.com>
2023-10-04 10:03:58 +0200

    SLING-12069: migrate to jakarta.json

--- src/test/java/org/apache/sling/servlets/post/JsonResponseTest.java
+++ src/test/java/org/apache/sling/servlets/post/JsonResponseTest.java
@@ -22,13 +22,12 @@ import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonNumber;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonString;
-import javax.json.JsonValue;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonNumber;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonString;
+import jakarta.json.JsonValue;
 import javax.servlet.http.HttpServletResponse;
 
 import junit.framework.TestCase;
